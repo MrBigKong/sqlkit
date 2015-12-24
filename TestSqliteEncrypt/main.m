5237b58c148e3d4d81fbf80a6bff4a2836c82260
yanlj <458260362@qq.com>
2015-12-24 16:20:50 +0800

    first commit

---
+++ TestSqliteEncrypt/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TestSqliteEncrypt
+//
+//  Created by yanluojun on 15/12/22.
+//  Copyright © 2015年 yanluojun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
