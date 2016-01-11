95a8bc5dc20ef2cb5d2e1cdd8c182b90e227172e
yanlj <458260362@qq.com>
2016-01-11 17:00:18 +0800

    commit sqlcipher

--- TestSqliteEncrypt/AppDelegate.m
+++ TestSqliteEncrypt/AppDelegate.m
@@ -7,10 +7,8 @@
 //
 
 #import "AppDelegate.h"
-#import <sqlite3.h>
 
 @interface AppDelegate ()
-@property (readonly) NSURL *databaseURL;
 
 @end
 
@@ -19,60 +17,10 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
-    [self openDB];
     
     return YES;
 }
 
-- (void)openDB {
-    // Set up a SQLCipher database connection:
-    sqlite3 *db;
-    if (sqlite3_open([[self.databaseURL path] UTF8String], &db) == SQLITE_OK) {
-        const char* key = [@"StrongPassword" UTF8String];
-        sqlite3_key(db, key, (int)strlen(key));
-        if (sqlite3_exec(db, (const char*) "SELECT count(*) FROM sqlite_master;", NULL, NULL, NULL) == SQLITE_OK) {
-            NSLog(@"Password is correct, or a new database has been initialized");
-        } else {
-            NSLog(@"Incorrect password!");
-        }
-        NSString *sql = @"CREATE TABLE IF NOT EXISTS testTable (id integer primary key AutoIncrement,col1 varchar(20),col2 varchar(20))";
-        if (sqlite3_exec(db, sql.UTF8String, NULL, NULL, NULL) == SQLITE_OK) {
-            NSLog(@"create table success");
-        } else {
-            NSLog(@"create table failure");
-        }
-
-        sqlite3_close(db);
-    }
-}
-
-- (NSURL *)databaseURL {
-    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
-    NSString *path = [paths objectAtIndex:0];
-    path = [path stringByAppendingString:@"/test.db"];
-    
-    NSFileManager *fm = [NSFileManager defaultManager];
-    //    [fm removeItemAtPath: path error: nil];
-    
-    if (![fm fileExistsAtPath:path isDirectory:NULL]) {
-        [fm copyItemAtPath:[[NSBundle mainBundle ] pathForResource:@"test.db" ofType:@""] toPath:path error:nil];
-    }
-    
-//    NSArray *URLs = [[NSFileManager defaultManager] URLsForDirectory:NSDocumentDirectory inDomains:NSUserDomainMask];
-//    NSURL *directoryURL = [URLs firstObject];
-    NSURL *databaseURL = [NSURL fileURLWithPath:path];
-    return  databaseURL;
-}
-
-- (BOOL)databaseExists {
-    BOOL exists = NO;
-    NSError *error = nil;
-    exists = [[self databaseURL] checkResourceIsReachableAndReturnError:&error];
-    if (exists == NO && error != nil) {
-        NSLog(@"Error checking availability of database file: %@", error);
-    }
-    return exists;
-}
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
--- TestSqliteEncrypt/ViewController.m
+++ TestSqliteEncrypt/ViewController.m
@@ -7,16 +7,89 @@
 //
 
 #import "ViewController.h"
+#import <sqlite3.h>
+#import "FMDatabase.h"
+#import "FMDatabaseQueue.h"
 
-@interface ViewController ()
+#define kTableTest @"TableTest"
+
+@interface ViewController () {
+    IBOutlet UITextField *_field;
+    IBOutlet UILabel *_logLabel;
+    sqlite3 *_db;
+}
+@property (nonatomic, strong) FMDatabaseQueue *databaseQueue;
+@property (readonly) NSURL *databaseURL;
 
 @end
 
 @implementation ViewController
 
+- (NSURL *)databaseURL {
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *path = [paths objectAtIndex:0];
+    path = [path stringByAppendingString:@"/test.db"];
+    
+    NSFileManager *fm = [NSFileManager defaultManager];
+    //    [fm removeItemAtPath: path error: nil];
+    
+    if (![fm fileExistsAtPath:path isDirectory:NULL]) {
+        [fm copyItemAtPath:[[NSBundle mainBundle ] pathForResource:@"test.db" ofType:@""] toPath:path error:nil];
+    }
+    
+    //    NSArray *URLs = [[NSFileManager defaultManager] URLsForDirectory:NSDocumentDirectory inDomains:NSUserDomainMask];
+    //    NSURL *directoryURL = [URLs firstObject];
+    NSURL *databaseURL = [NSURL fileURLWithPath:path];
+    return  databaseURL;
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    [self openDB];
+    
+    
+}
+
+- (void)openDB {
+    // Set up a SQLCipher database connection:
+    if (sqlite3_open([[self.databaseURL path] UTF8String], &_db) == SQLITE_OK) {
+        const char* key = [@"123456" UTF8String];
+        sqlite3_key(_db, key, (int)strlen(key));
+        if (sqlite3_exec(_db, (const char*) "SELECT count(*) FROM sqlite_master;", NULL, NULL, NULL) == SQLITE_OK) {
+            _logLabel.text = @"密码正确，可以开始操作数据库";
+        } else {
+            _logLabel.text = @"密码错误,打开数据库失败";
+        }
+        NSString *sql = [NSString stringWithFormat:@"CREATE TABLE IF NOT EXISTS %@ (id integer primary key AutoIncrement,col1 varchar(20),col2 varchar(20))",kTableTest];
+        if (sqlite3_exec(_db, sql.UTF8String, NULL, NULL, NULL) == SQLITE_OK) {
+//            NSLog(@"create table success");
+        } else {
+//            NSLog(@"create table failure");
+        }
+    }
+}
+
+- (void)initFMDB {
+    self.databaseQueue = [FMDatabaseQueue databaseQueueWithPath:[self.databaseURL absoluteString]];
+
+}
+
+-(IBAction)querySQLAction:(id)sender {
+//    NSString *sql = [NSString stringWithFormat:@"SELECT * FROM TABLE %@",kTableTest];
+//    
+//    NSMutableArray *ar = [[NSMutableArray alloc] initWithCapacity:1];
+//    
+//    [self.databaseQueue inDatabase:^(FMDatabase *database){
+//        FMResultSet *rs = [database executeQuery:sql];
+//        while ([rs next]) {
+//            [ar addObject:[rs resultDictionary]];
+//        };
+//        [rs close];
+//    }];
+//    
+//    return result;
+//    
 }
 
 - (void)didReceiveMemoryWarning {
